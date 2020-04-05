5f83724e1c7e65c33653624204c7f319fc15ba74
Sitthidate Nanuea <Sitthidate.N@draycir.com>
2020-04-05 21:13:05 +0700

    Add canvas with material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { fabric } from "fabric";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  itemName:string;
+  canvas: any;
+  width:number = 800-32;
+  height:number = 600;
+
+
+  ngOnInit(): void {
+    this.canvas = new fabric.Canvas("c", {
+      centeredScaling: true,
+      backgroundColor: 'rgba(248,248,248)',
+      selection: true
+    });
+
+    this.canvas.setWidth(this.width);
+    this.canvas.setHeight(this.height);
+    
+    
+    this.canvas.add(new fabric.IText('Hello Fabric!'));
+    this.addText('hello');
+    this.addText('hello2');
+  }
+
+  addText(text) {
+    var fabricText = new fabric.IText(text, {
+      left: (Math.random()*0.8+0.1) * this.width,
+      top: (Math.random()*0.8+0.1) * this.height,
+      fill: '#000',
+      angle: Math.random()*30 - 15
+    });
+    this.canvas.add(fabricText);
+  }
+
+  add() {
+    console.log('add ' + this.itemName);
+    this.addText(this.itemName);
+    this.itemName = "";
+  }
+
   title = 'canvas-app';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MaterialModule} from './material-module';
+import {MatNativeDateModule} from '@angular/material/core';
+import {MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,9 +16,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [
+    { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'fill' } },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
